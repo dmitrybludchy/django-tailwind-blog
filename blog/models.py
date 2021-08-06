c85055345be04b8c03cd387faffd061e1a9a249e
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-08-06 18:37:20 +0300

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Категория')
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Тег')
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model): 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=250, verbose_name='Название статьи')
+    slug = models.SlugField(max_length=250, unique_for_date='publish',verbose_name='Url')
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото')
+    body = models.TextField(verbose_name='Содержание')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    status = models.BooleanField(default=True, verbose_name='Статус')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, blank=True,related_name='posts', verbose_name='Тег')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
