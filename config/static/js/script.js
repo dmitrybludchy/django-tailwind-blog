625eebd897c20abf70e5122265d80d111f4ca650
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-08-10 19:18:31 +0300

    add template

--- config/static/js/script.js
+++ config/static/js/script.js
@@ -1,40 +1,23 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
-
-    if (burger.length && menu.length) {
-        for (let i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (let j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
+window.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('#burger');
+    const menu = document.querySelector('#menu');
+  
+    const showHide = function(el, sh) {
+      el.addEventListener('click', () => {
+        if (sh.style.display === "none") {
+          sh.style.display = "";
+        } else {
+          sh.style.display = "none";
         }
-    }
+      })
+    };
+  
+    showHide(burger, menu);
+  
 
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (let i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (let j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (let i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (let j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+    const btnComponents = document.querySelector('#components');
+    const btnComponentsOn = document.querySelector('#componentsOn');
+  
+    showHide(btnComponents, btnComponentsOn);
+  
 });
