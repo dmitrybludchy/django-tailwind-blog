637d8a662d48cd356546cc10e1e87abb67a98e0b
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-08-12 13:01:36 +0300

    add change template and add post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>', get_category, name='category'),
+    path('category/<str:slug>', PostsByCategory.as_view(), name='category'),
+    path('post/<str:slug>', GetPost.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from typing import List
 from django.shortcuts import render
 from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
+from django.db.models import F
 
 
 class Home(ListView):
@@ -10,13 +12,27 @@ class Home(ListView):
     paginate_by = 6
 
 
-def index(request):
-    articles = Post.objects.all()
-    context = {
-        'articles': articles,
-    }
-    return render(request, 'blog/index.html', context)
+class PostsByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
 
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+      
+
+
